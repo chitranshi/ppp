2595dc88354c35ffc7af87bda617517c068e75cc
Maykel Moya <moya@latertulia.org>
2009-12-18 19:02:59 -0500

    Consider env DEVICE when looking for tty2port mapping
    When the option man-to-ttyname is given pppd will look in the
    tty-to-port mapping using the real device name. This change that
    behaviour to prefer using the DEVICE env variable if present.

--- pppd/plugins/radius/radius.c
+++ pppd/plugins/radius/radius.c
@@ -89,6 +89,7 @@ static void radius_choose_ip(u_int32_t *addrp);
 static int radius_init(char *msg);
 static int get_client_port(char *ifname);
 static char *get_caller_id();
+static char *get_device_name();
 static int radius_allowed_address(u_int32_t addr);
 static void radius_acct_interim(void *);
 #ifdef MPPE
@@ -273,7 +274,7 @@ radius_pap_auth(char *user,
 
     /* Hack... the "port" is the ppp interface number.  Should really be
        the tty */
-    rstate.client_port = get_client_port(portnummap ? devnam : ifname);
+    rstate.client_port = get_client_port(portnummap ? get_device_name() : ifname);
 
     av_type = PW_FRAMED;
     rc_avpair_add(&send, PW_SERVICE_TYPE, &av_type, 0, VENDOR_NONE);
@@ -375,7 +376,7 @@ radius_chap_verify(char *user, char *ourname, int id,
     /* Put user with potentially realm added in rstate.user */
     if (!rstate.done_chap_once) {
 	make_username_realm(user);
-	rstate.client_port = get_client_port (portnummap ? devnam : ifname);
+	rstate.client_port = get_client_port (portnummap ? get_device_name() : ifname);
 	if (radius_pre_auth_hook) {
 	    radius_pre_auth_hook(rstate.user,
 				 &rstate.authserver,
@@ -1300,6 +1301,26 @@ get_caller_id()
 		return remote_number;
 
 	return NULL;
+}
+
+/**********************************************************************
+* %FUNCTION: get_device_name()
+* %ARGUMENTS:
+*  None
+* %RETURNS:
+*  The device name
+* %DESCRIPTION:
+*  Returns devnam unless DEVICE is present in the environment
+***********************************************************************/
+static char*
+get_device_name()
+{
+	char *device_name;
+	device_name = getenv("DEVICE");
+	if (device_name != NULL)
+		return device_name;
+	else
+		return devnam;
 }
 
 /**********************************************************************
